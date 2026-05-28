aac36f704ab7cbfb00410a28e38283f166ef8fb7
Rakibul Hasan <rakibulhasan745101@gmail.com>
2026-05-28 23:19:19 +0600

    Creation of user and issue table into the database

---
+++ src/db/index.ts
@@ -0,0 +1,48 @@
+import { Pool } from "pg";
+import env_variables from "../config/env_variables";
+
+const pool = new Pool({
+    connectionString: env_variables.db_url
+})
+
+export const initDb = async () => {
+
+    try {
+        await pool.query(
+            `
+                CREATE TABLE IF NOT EXISTS users (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(25) NOT NULL,
+                    email VARCHAR(32) UNIQUE NOT NULL,
+                    password TEXT NOT NULL,
+                    role VARCHAR(16) DEFAULT 'contributor',
+
+                    created_at TIMESTAMP DEFAULT NOW(),
+                    updated_ata TIMESTAMP DEFAULT NOW()
+                )
+            `
+        )
+
+        await pool.query(
+            `
+                CREATE TABLE IF NOT EXISTS issues (
+                    id SERIAL PRIMARY KEY,
+                    title VARCHAR(150) NOT NULL,
+                    description TEXT NOT NULL,
+                    type VARCHAR(16) NOT NULL,
+                    status VARCHAR(16) NOT NULL,
+                    reporter_id INT REFERENCES users(id) ON DELETE CASCADE,
+
+                    created_at TIMESTAMP DEFAULT NOW(),
+                    updated_at TIMESTAMP DEFAULT NOW()
+                )
+            `
+        )
+        console.log("Database connection is successful! 🙌");
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default pool;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app";
 import env_variables from "./config/env_variables";
+import { initDb } from "./db";
 
-app.listen(env_variables.port, () => {
+app.listen(env_variables.port,  () => {
+    initDb()
     console.log(`Server is running at port: ${env_variables.port}`);
-    
 })
